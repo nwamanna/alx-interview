f055ed35a28cbd86dadaef8ce080608fa6179334
Nwamanna <nwamannazika@gmail.com>
2023-10-03 19:58:06 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+    # Initialize a list to store the keys found
+    eleList = set()
+    eleList.add(0)
+    
+    # Iterate through each box
+    for box in boxes:
+        for element in box:
+            # Append each key found to eleList
+            eleList.add(element)
+    
+    print (eleList)
+    # Iterate through each box index
+    for idx in range(len(boxes)):
+        # Check if the index is in eleList (if we have the key)
+        if idx not in eleList:
+            return False  # If any box is not reachable, return False
+    
+    return True  
