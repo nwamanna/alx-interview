13aa6c9a811a0e635af456c39f4b003459725688
Nwamanna <nwamannazika@gmail.com>
2023-10-22 17:56:49 +0100

    fix bug

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,37 @@
 #!/usr/bin/python3
-"""log parsing"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
-import re
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def match(x: str) -> bool:
-    """Checks for input match"""
-    ip_match = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-    date_match = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\] '
-    stats_match = r'"GET /projects/260 HTTP/1\.1" \d{3} \d+'
-    pattern = ip_match + date_match + stats_match
-    return True if re.match(pattern, x) else False
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def log_parse() -> None:
-    """ A function that reads stdin line by line and computes metrics """
-    while True:
-        usage = '<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>'
-        line_count = 0
-        total_f_size = 0
-        status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-        status_code_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-        try:
-            for line in sys.stdin:
-                if not match(line):
-                    continue  # Skip lines that don't match the format
+except Exception as err:
+    pass
 
-                a, b, c, d, e, f, g, status_code, f_size = line.split()
-                total_f_size += int(f_size)
-
-                # Convert status_code to an integer
-                status_code = int(status_code)
-
-                if status_code in status_codes:
-                    status_code_dict[str(status_code)] += 1
-
-                line_count += 1
-
-                if line_count % 10 == 0:
-                    print(f'File size: {total_f_size}')
-                    for key, val in status_code_dict.items():
-                        if val > 0:
-                            print(f"{key}: {val}")
-
-        except KeyboardInterrupt:
-            print(f'File size: {total_f_size}')
-            for key, val in status_code_dict.items():
-                if val > 0:
-                    print(f"{key}: {val}")
-            exit(0)
-
-
-if __name__ == '__main__':
-    log_parse()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
